388ed9f490e2cfd1eb839c9636fd8bd92d5db430
Marco H <58659368+Marco12223@users.noreply.github.com>
2024-04-18 21:53:18 +0200

    Creating all important stuff

---
+++ src/main/java/de/marco1223/strawberry/commands/music/stopCommand.java
@@ -0,0 +1,64 @@
+package de.marco1223.strawberry.commands.music;
+
+import de.marco1223.strawberry.Strawberry;
+import de.marco1223.strawberry.handlers.api.LanguageHandler;
+import de.marco1223.strawberry.handlers.music.QueueHandler;
+import de.marco1223.strawberry.interfaces.SlashCommandInterface;
+import de.marco1223.strawberry.localizations.music.stopCoammdLocalizations;
+import de.marco1223.strawberry.utils.EmbedPattern;
+import dev.arbjerg.lavalink.client.player.LavalinkPlayer;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.localization.LocalizationFunction;
+import org.jetbrains.annotations.NotNull;
+
+public class stopCommand implements SlashCommandInterface {
+    @Override
+    public void execute(@NotNull SlashCommandInteractionEvent event) {
+
+        event.deferReply(true).queue();
+        String lang = LanguageHandler.getGuildLocale(event.getGuild().getId());
+
+        if(event.getGuild().getSelfMember().getVoiceState().getChannel() != null) {
+
+            if(event.getMember().getVoiceState().getChannel() != null && event.getGuild().getSelfMember().getVoiceState().getChannel().getIdLong() == event.getMember().getVoiceState().getChannel().getIdLong()) {
+
+                LavalinkPlayer player = Strawberry.getLavalinkClient().getOrCreateLink(event.getGuild().getIdLong()).getPlayer().block();
+
+                if(player.getTrack() != null) {
+                    QueueHandler queueHandler = new QueueHandler(event.getGuild().getIdLong());
+
+                    queueHandler.clearQueue();
+                    player.stopTrack().subscribe();
+                    player.setTrack(null).subscribe();
+                    event.getGuild().getAudioManager().closeAudioConnection();
+
+                    event.getHook().sendMessageEmbeds(EmbedPattern.info(LanguageHandler.Language(lang, "values.stopCommand.embed.success.title"), LanguageHandler.Language(lang, "values.stopCommand.embed.success.description"), null, event.getUser().getAvatarUrl(), null, null, null)).queue();
+
+                } else {
+                    event.getHook().sendMessageEmbeds(EmbedPattern.error(LanguageHandler.Language(lang, "values.stopCommand.embed.errors.noTrack.title"), LanguageHandler.Language(lang, "values.stopCommand.embed.errors.noTrack.description"), null, event.getUser().getAvatarUrl(), null, null, null)).queue();
+                }
+
+            } else {
+                event.getHook().sendMessageEmbeds(EmbedPattern.error(LanguageHandler.Language(lang, "values.stopCommand.embed.errors.notInSameVoiceChannel.title"), LanguageHandler.Language(lang, "values.stopCommand.embed.errors.notInSameVoiceChannel.description"), null, event.getUser().getAvatarUrl(), null, null, null)).queue();
+            }
+
+        } else {
+            event.getHook().sendMessageEmbeds(EmbedPattern.error(LanguageHandler.Language(lang, "values.stopCommand.embed.errors.noVoiceChannel.title"), LanguageHandler.Language(lang, "values.stopCommand.embed.errors.noVoiceChannel.description"), null, event.getUser().getAvatarUrl(), null, null, null)).queue();
+        }
+
+    }
+
+    @NotNull
+    @Override
+    public CommandData getCommandData() {
+
+        LocalizationFunction localizations = new stopCoammdLocalizations();
+
+        return Commands.slash("stop", LanguageHandler.Language("en-US", "values.stopCommand.description"))
+                .setGuildOnly(true)
+                .setLocalizationFunction(localizations);
+
+    }
+}
