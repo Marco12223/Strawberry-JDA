aa9e496434f4c6f6fc7b0fa2b7015056dbe570d2
Marco H <58659368+Marco12223@users.noreply.github.com>
2024-04-23 16:20:28 +0200

    Add guilds, users on Shard and music players

--- src/main/java/de/marco1223/strawberry/commands/system/pingCommand.java
+++ src/main/java/de/marco1223/strawberry/commands/system/pingCommand.java
@@ -1,15 +1,19 @@
 package de.marco1223.strawberry.commands.system;
 
+import de.marco1223.strawberry.Strawberry;
 import de.marco1223.strawberry.handlers.api.LanguageHandler;
 import de.marco1223.strawberry.interfaces.SlashCommandInterface;
 import de.marco1223.strawberry.localizations.system.pingCommandLocalizations;
 import de.marco1223.strawberry.utils.EmbedPattern;
+import dev.arbjerg.lavalink.client.player.LavalinkPlayer;
+import dev.arbjerg.lavalink.protocol.v4.Player;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.localization.LocalizationFunction;
 import org.jetbrains.annotations.NotNull;
+import reactor.core.Disposable;
 
 import java.util.List;
 
@@ -21,7 +25,10 @@ public class pingCommand implements SlashCommandInterface {
 
         List<MessageEmbed.Field> fields = List.of(
                 new MessageEmbed.Field(LanguageHandler.Language(lang, "values.pingCommand.embed.fields.ws"), event.getJDA().getGatewayPing() + "ms", true),
-                new MessageEmbed.Field(LanguageHandler.Language(lang, "values.pingCommand.embed.fields.rest"), event.getJDA().getRestPing().complete() + "ms", true)
+                new MessageEmbed.Field(LanguageHandler.Language(lang, "values.pingCommand.embed.fields.rest"), event.getJDA().getRestPing().complete() + "ms", true),
+                new MessageEmbed.Field("Guilds size on Shard", event.getJDA().getGuilds().size() + "Guilds", true),
+                new MessageEmbed.Field("Users size on Shard", event.getJDA().getUsers().size() + "Users", true),
+                new MessageEmbed.Field("Music players", String.valueOf(Strawberry.getLavalinkClient().getNodes().get(0).getPlayers().block().size()), true)
         );
 
         event.getHook().sendMessageEmbeds(EmbedPattern.info(LanguageHandler.Language(lang, "values.pingCommand.embed.title").replace("{shardId}", event.getJDA().getShardInfo().getShardId()+1 + "/" + event.getJDA().getShardInfo().getShardTotal()), LanguageHandler.Language(lang, "values.pingCommand.embed.description"), null, event.getUser().getAvatarUrl(), fields, null, null)).queue();
