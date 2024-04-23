96fda04efca4818178d3c913213a5d711dc8aeba
Marco H <58659368+Marco12223@users.noreply.github.com>
2024-04-23 21:14:54 +0200

    Fixing bug `Player won't destroy`

--- src/main/java/de/marco1223/strawberry/listeners/music/DestoryLinkOnLeaveListener.java
+++ src/main/java/de/marco1223/strawberry/listeners/music/DestoryLinkOnLeaveListener.java
@@ -1,7 +1,6 @@
 package de.marco1223.strawberry.listeners.music;
 
 import de.marco1223.strawberry.Strawberry;
-import de.marco1223.strawberry.handlers.music.QueueHandler;
 import dev.arbjerg.lavalink.client.Link;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceUpdateEvent;
 import net.dv8tion.jda.api.hooks.EventListener;
@@ -11,14 +10,9 @@ public class DestoryLinkOnLeaveListener extends ListenerAdapter implements Event
 
     @Override
     public void onGuildVoiceUpdate(GuildVoiceUpdateEvent event) {
-        if(event.getMember().getIdLong() == event.getGuild().getSelfMember().getIdLong() || event.getChannelLeft() != null) {
+        if(event.getMember().getIdLong() == event.getGuild().getSelfMember().getIdLong() && event.getChannelLeft() != null) {
             Link link = Strawberry.getLavalinkClient().getOrCreateLink(event.getGuild().getIdLong());
-            QueueHandler queueHandler = new QueueHandler(event.getGuild().getIdLong());
-            queueHandler.clearQueue();
-            link.updatePlayer((update) -> {
-                update.setTrack(null).setPaused(false);
-            }).subscribe();
-            link.destroy();
+            link.getNode().destroyPlayerAndLink(link.getGuildId());
         }
     }
 }
